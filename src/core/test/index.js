cf6e9e122ed0db0e467341bbb9113b656a224683
George Fogle <fogle.george@gmail.com>
2018-12-13 21:31:01 -0500

    spike: basic assertions (#17)

--- src/core/test/index.js
+++ src/core/test/index.js
@@ -1,4 +1,28 @@
-class ActiveTest {}
+const assert = require("assert");
+
+class ActiveTest {
+  constructor() {}
+
+  assert(val) {
+    return {
+      equals: function(other, msg) {
+        return assert.strictEqual(val, other, msg);
+      },
+      deepEquals: function(other, msg) {
+        return assert.deepStrictEqual(val, other, msg);
+      },
+      notEqualTo: function(other, msg) {
+        return assert.notStrictEqual(val, other, msg);
+      },
+      notDeepEqualTo: function(other, msg) {
+        return assert.notDeepStrictEqual(val, other, msg);
+      },
+      isTruthy: function() {
+        return assert.ok(val);
+      }
+    };
+  }
+}
 
 module.exports = {
   ActiveTest
