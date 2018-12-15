92f72001a76de7320287fd9ab586011437473b39
George Fogle <fogle.george@gmail.com>
2018-12-15 09:56:39 -0500

    chore: rename files to their class definitions (#22)

--- src/core/test/index.js
+++ src/core/test/index.js
@@ -1,30 +1,4 @@
-const assert = require("assert");
-
-class ActiveTest {
-  constructor() {}
-
-  assert(val) {
-    return {
-      equals: function(other, msg) {
-        return assert.strictEqual(val, other, msg);
-      },
-      deepEquals: function(other, msg) {
-        return assert.deepStrictEqual(val, other, msg);
-      },
-      notEqualTo: function(other, msg) {
-        return assert.notStrictEqual(val, other, msg);
-      },
-      notDeepEqualTo: function(other, msg) {
-        return assert.notDeepStrictEqual(val, other, msg);
-      },
-      isTruthy: function() {
-        return assert.ok(val);
-      }
-    };
-  }
-}
-
 module.exports = {
-  ActiveTest,
+  ActiveTest: require("./ActiveTest"),
   IntegrationTest: require("./IntegrationTest")
 };
