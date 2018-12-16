397b715ab1296c8ad80575f7bb53c12fc12446df
George Fogle <fogle.george@gmail.com>
2018-12-15 22:02:15 -0500

    jsdoc of db file

--- src/core/db/index.js
+++ src/core/db/index.js
@@ -1,4 +1,8 @@
 module.exports = {
+  /**
+   * @description get current db connection for running web application
+   * @returns the knex handle to the database for a web application
+   */
   connection: function() {
     const db = require(`${process.cwd()}/db`);
 
