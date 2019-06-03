6056ad28e137ab9321b90af257f4f35f82ecd825
George Fogle <fogle.george@gmail.com>
2019-06-03 07:53:58 -0400

    chore: lint command to package.json

--- src/cli/new.js
+++ src/cli/new.js
@@ -37,6 +37,7 @@ module.exports = function(name, root) {
       {
         name: name,
         scripts: {
+          lint: "./node_modules/.bin/eslint --color app config test",
           start: "./node_modules/.bin/boring server",
           test: "./node_modules/.bin/boring test"
         },
