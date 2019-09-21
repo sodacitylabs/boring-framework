5ded7b7e52e6ac44c2377aa827fa929c2b3a994c
George Fogle <fogle.george@gmail.com>
2019-09-21 00:29:14 -0400

    wip

--- src/core/helpers/AssetHelper.js
+++ src/core/helpers/AssetHelper.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const UrlHelper = require("./UrlHelper");
 
-let robots;
-
 module.exports = class AssetHelper {
   /**
    * @description returns a map of file extensions to their MIME types
